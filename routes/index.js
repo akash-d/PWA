be03d7962a7d7e40b602603f2055130d5d63f27f
soumitra mishra <Soumitra.Mishra@go-mmt.com>
2018-04-27 04:02:49 +0530

    BT-1 fixing routes issue

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,32 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 var STATIC_MAPPINGS = {
-    '/about.html': 'about',
-    '/contact.html': 'contact',
-    '/index.html': 'index',
-    '/services.html': 'services',
-    '/work.html': 'work',
-    '/error.html': 'error'
+    '/about': 'about',
+    '/contact': 'contact',
+    '/index': 'index',
+    '/services': 'services',
+    '/work': 'work',
+    '/error': 'error'
 }
-var STATIC_ROUTES = ['/about.html', '/contact.html', '/index.html', '/services.html', '/work.html', '/error.html'];
-for (var route in STATIC_ROUTES) {
-    router.get(STATIC_ROUTES[route], function(req, res) {
-        res.render(STATIC_MAPPINGS[STATIC_ROUTES[route]]);
-    })
-}
+var STATIC_ROUTES = ['/about', '/contact', '/index.html', '/services', '/work', '/error'];
+
+router.get(STATIC_ROUTES[0], function(req, res) {
+    res.render(STATIC_MAPPINGS[STATIC_ROUTES[0]]);
+});
+router.get(STATIC_ROUTES[1], function(req, res) {
+    res.render(STATIC_MAPPINGS[STATIC_ROUTES[1]]);
+});
+router.get(STATIC_ROUTES[2], function(req, res) {
+    res.render(STATIC_MAPPINGS[STATIC_ROUTES[2]]);
+});
+router.get(STATIC_ROUTES[3], function(req, res) {
+    res.render(STATIC_MAPPINGS[STATIC_ROUTES[3]]);
+});
+router.get(STATIC_ROUTES[4], function(req, res) {
+    res.render(STATIC_MAPPINGS[STATIC_ROUTES[4]]);
+});
+router.get(STATIC_ROUTES[5], function(req, res) {
+    res.render(STATIC_MAPPINGS[STATIC_ROUTES[5]]);
+});
+
 module.exports = router;
